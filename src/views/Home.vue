1ff5656eea7e5c8bad2ae950687816abdd8b7f6a
wealipay <1293600794@qq.com>
2025-07-26 05:57:52 +0800

    hhj

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,7 +7,6 @@
 <script setup lang="ts">
   import { ref } from 'vue'
   import Hyperspeed from '@/components/bits/Hyperspeed.vue'
-  import { hyperspeedPresets } from '@/components/bits/HyperspeedPresets'
 
   // Use a preset or create custom options
   const effectOptions = ref(hyperspeedPresets.one)
