9857250592151ae1af53b056656ea5d9b4b23d21
wealipay <1293600794@qq.com>
2025-07-27 05:04:20 +0800

    hhh

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,8 +2,8 @@
   <div class="relative w-full h-full">
     <Orb className="w-full h-full absolute left-1/2 -translate-x-1/2  top-1/2 transform -translate-y-1/2" :hoverIntensity="3.65" :rotateOnHover="false" :hue="237" :forceHoverState="false" />
     <CircularText
-    className="text-blue-500 absolute  top-1/2 transform -translate-y-1/2"
-    text=" 欢 迎ggg 光 临 alipay.wealipay.top"
+    className="text-blue-500 bg-white absolute  top-1/2 transform -translate-y-1/2"
+    text=" 欢 迎 光 临 alipay.wealipay.top"
     :spin-duration="20"
     on-hover="speedUp"
   />
