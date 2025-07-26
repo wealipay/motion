2659db4da47e4de23380ac9c9ba768babfaef1a5
wealipay <1293600794@qq.com>
2025-07-27 06:42:14 +0800

    hhh

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="relative w-full h-full">
     <Orb className="w-full h-full absolute left-1/2 -translate-x-1/2  top-1/2 transform -translate-y-1/2" :hoverIntensity="3.65" :rotateOnHover="false" :hue="237" :forceHoverState="false" />
-    <a href="javascript:alert('这是一个提示框')"><CircularText
+    <a href="javascript:alert('点击环绕光圈穿越时空！')"><CircularText
     className="font-roboto text-blue-500  absolute  top-1/2 transform -translate-y-1/2"
     text=" 欢 迎g 光 临 alipay.wealipay.top"
     :spin-duration="20"
