ee8c6c50b3f6dcdfe14c40f4f3b4b722b34012d4
wealipay <1293600794@qq.com>
2025-07-26 19:31:56 +0800

    ghh

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="relative w-full h-full">
-    <Orb :hoverIntensity="0.5" :rotateOnHover="false" :hue="0" :forceHoverState="false" />
+    <Orb :hoverIntensity="3.65" :rotateOnHover="false" :hue="237" :forceHoverState="false" />
     <div className="mx-auto">alipay.wealipay.top</div>
   </div>
 </template>
