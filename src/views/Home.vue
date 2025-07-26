c341f4e6a1599522c5ed367fc4e969366188a66e
wealipay <1293600794@qq.com>
2025-07-26 21:52:55 +0800

    ghh

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="relative w-full h-full">
     <Orb :hoverIntensity="3.65" :rotateOnHover="false" :hue="237" :forceHoverState="false" />
-    <div className="mx-auto">欢迎</div>
+    <CircularText
+    text="欢迎 * 来到 * alipay.wealipay.top * "
+    :spin-duration="20"
+    on-hover="speedUp"
+    class-name="text-blue-500"
+  />
   </div>
 </template>
 
 <script setup lang="ts">
   import Orb from "@/components/bits/Orb.vue";
+  import CircularText from "@/components/bits/CircularText.vue";
 </script>
