87ed8f5ab6ecf2d2c1acb07eac40a252a3131a55
wealipay <1293600794@qq.com>
2025-07-29 10:12:19 +0800

    hhhh

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,32 @@
 <template>
   <div class="relative w-full h-full">
-    <Orb className="w-full h-full absolute left-1/2 -translate-x-1/2  top-1/2 transform -translate-y-1/2" :hoverIntensity="3.65" :rotateOnHover="false" :hue="237" :forceHoverState="false" />
-    <a href="javascript:alert('点击环绕光圈穿越时空！')"><CircularText
-    className="font-roboto text-blue-500  absolute  top-1/2 transform -translate-y-1/2"
-    text=" 欢 迎g 光 临 alipay.wealipay.top"
-    :spin-duration="20"
-    on-hover="speedUp"
-  /></a>
+    <div @click="handdledelay">
+      <Orb
+        className="w-full h-full absolute left-1/2 -translate-x-1/2  top-1/2 transform -translate-y-1/2"
+        :hoverIntensity="3.65"
+        :rotateOnHover="false"
+        :hue="237"
+        :forceHoverState="false"
+      />
+    </div>
+    <a href="javascript:alert('点击环绕光圈穿越时空！')"
+      ><CircularText
+        className="font-roboto text-blue-500  absolute  top-1/2 transform -translate-y-1/2"
+        text=" 欢 迎g 光 临 alipay.wealipay.top"
+        :spin-duration="20"
+        on-hover="speedUp"
+    /></a>
   </div>
 </template>
 
 <script setup lang="ts">
-  import Orb from "@/components/bits/Orb.vue";
-  import CircularText from "@/components/bits/CircularText.vue";
+import {useRouter} from "vue";
+import Orb from "@/components/bits/Orb.vue";
+import CircularText from "@/components/bits/CircularText.vue";
+const handdledelay=()=>{
+setTimeout(()=>{
+const router=useRouter()
+router.push("www.baidu.com")
+},2000)
+}
 </script>
