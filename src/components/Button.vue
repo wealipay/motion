13ec4fe4676461b3513d6e43d8bd0d6a91cf4959
wealipay <1293600794@qq.com>
2025-08-05 18:49:02 +0800

    hhh

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -24,6 +24,6 @@ const getSize = () => {
     :colors="['#ffaa40', '#9c40ff', '#ffaa40']"
     :animation-speed="8"
     :show-border="true"
-    className="w-48 text-white"
+    class="w-48 p-8"
   />
 </template>
